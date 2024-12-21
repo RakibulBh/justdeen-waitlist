b47bd835b8d7ad22522c3d9a1156efae4b5c3f23
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-12-21 22:06:54 +0000

    add: better user input experience

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 
-const Button = ({ children }: { children: React.ReactNode }) => {
+const Button = ({
+  children,
+  disabled,
+}: {
+  children: React.ReactNode;
+  disabled?: boolean;
+}) => {
   return (
-    <button className="px-4 sm:px-8 py-3 rounded-lg bg-gradient-to-r from-[#9f45b0] to-[#e54ed0] text-white text-xs">
+    <button
+      disabled={disabled}
+      className="px-4 sm:px-8 py-3 rounded-lg bg-gradient-to-r from-[#9f45b0] to-[#e54ed0] text-white text-xs"
+    >
       {children}
     </button>
   );
